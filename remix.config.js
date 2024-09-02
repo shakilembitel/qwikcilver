20e8f08f83bb449ba8b167fc8768ac79adf68e4f
emb-mohaala <mohammad.alam@embitel.com>
2024-09-02 17:15:29 +0530

    add dotnv

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
+
+require("dotenv").config();
+
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -17,4 +20,5 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
+  serverDependenciesToBundle: [/^remix-utils.*/],
 };
